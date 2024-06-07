01b3f95899d71d28521ed3da1edee72fdd63b890
Niklas Jensen <niklaspj2004@gmail.com>
2024-06-07 15:47:16 +0200

    Properly returns lobbyid on players request

--- src/main/java/dk/dtu/compute/se/pisd/roborallyserver/model/ServerPlayer.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborallyserver/model/ServerPlayer.java
@@ -1,15 +1,21 @@
 package dk.dtu.compute.se.pisd.roborallyserver.model;
 
 import java.time.LocalDate;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,7 +37,13 @@ public class ServerPlayer {
 
     @ManyToOne
     @JoinColumn(name = "lobby_id", nullable = false)
+    @JsonIgnore
     private Lobby lobby;
 
+    // Only return the lobby id and not the lobby object itself
+    @Transient
+    public Long getLobbyId() {
+        return lobby.getId();
+    }
 
 }
