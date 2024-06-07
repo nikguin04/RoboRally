e4820839a77c358734477bfcf47e041a34636ba4
Niklas Jensen <niklaspj2004@gmail.com>
2024-06-07 13:21:28 +0200

    Save with post request working for lobby

--- src/main/java/dk/dtu/compute/se/pisd/roborallyserver/controller/LobbyController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborallyserver/controller/LobbyController.java
@@ -4,9 +4,12 @@ package dk.dtu.compute.se.pisd.roborallyserver.controller;
 import dk.dtu.compute.se.pisd.roborallyserver.model.Lobby;
 import dk.dtu.compute.se.pisd.roborallyserver.repository.LobbyRepository;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +34,11 @@ public class LobbyController {
         return ResponseEntity.ok(lobbyList);
     }
 
+	@PostMapping("/newlobby")
+	public ResponseEntity<Lobby> newLobby(@RequestBody Lobby lobby) {
 
+        lobbyRepository.saveAndFlush(lobby);
+		return ResponseEntity.ok(lobby); // lobbyRepository.save(lobby)
+
+	}
 }
