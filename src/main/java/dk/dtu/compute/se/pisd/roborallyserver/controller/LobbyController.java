aa810c034924ff99478962ea3d32ae9bfef9c88a
Niklas Jensen <niklaspj2004@gmail.com>
2024-06-09 18:02:07 +0200

    Select all player by lobbyid (query added)

--- src/main/java/dk/dtu/compute/se/pisd/roborallyserver/controller/LobbyController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborallyserver/controller/LobbyController.java
@@ -2,7 +2,9 @@ package dk.dtu.compute.se.pisd.roborallyserver.controller;
 
 
 import dk.dtu.compute.se.pisd.roborallyserver.model.Lobby;
+import dk.dtu.compute.se.pisd.roborallyserver.model.ServerPlayer;
 import dk.dtu.compute.se.pisd.roborallyserver.repository.LobbyRepository;
+import dk.dtu.compute.se.pisd.roborallyserver.repository.PlayerRepository;
 
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +23,11 @@ import java.util.List;
 public class LobbyController {
 
     private LobbyRepository lobbyRepository;
+    private PlayerRepository playerRepository;
 
-    public LobbyController(LobbyRepository lobbyRepository) {
+    public LobbyController(LobbyRepository lobbyRepository, PlayerRepository playerRepository) {
         this.lobbyRepository = lobbyRepository;
+        this.playerRepository = playerRepository;
     }
 
     @GetMapping
@@ -32,6 +36,12 @@ public class LobbyController {
     public ResponseEntity<List<Lobby>> getLobbies(){
         List<Lobby> lobbyList = lobbyRepository.findAll();
         return ResponseEntity.ok(lobbyList);
+    }
+
+    @GetMapping("/players")
+    public ResponseEntity<List<ServerPlayer>> getPlayersInLobby(){
+        List<ServerPlayer> playerList = playerRepository.findByLobbyID(Long.valueOf(1));
+        return ResponseEntity.ok(playerList);
     }
 
 	@PostMapping("/newlobby")
--- src/main/java/dk/dtu/compute/se/pisd/roborallyserver/repository/PlayerRepository.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborallyserver/repository/PlayerRepository.java
@@ -1,5 +1,7 @@
 package dk.dtu.compute.se.pisd.roborallyserver.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +11,10 @@ import dk.dtu.compute.se.pisd.roborallyserver.model.ServerPlayer;
 
 public interface PlayerRepository extends JpaRepository<ServerPlayer, Long> {
 	public ServerPlayer findPlayerById(long id);
+
+
+	//https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+	@Query("select p from ServerPlayer p where p.lobby.id = ?1")
+	public List<ServerPlayer> findByLobbyID(Long id);
+
 }
