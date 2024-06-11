1eeda166414a57ef28d50f6c335a10cb3c2618d1
Christian Livbjerg Worsøe <s235111@dtu.dk>
2024-06-11 12:42:48 +0200

    Fix Map server stuff

--- src/main/java/dk/dtu/compute/se/pisd/roborallyserver/model/Map.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborallyserver/model/Map.java
@@ -1,7 +1,6 @@
 package dk.dtu.compute.se.pisd.roborallyserver.model;
 
 import java.io.Serializable;
-import java.util.List;
 
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
@@ -9,7 +8,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,17 +20,16 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
-public class Maps implements Serializable {
+public class Map implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String mapstring;
+	private String mapstring;
 
-    private Long numberofplayers;
+	private Long numberofplayers;
 
-    private String mapname;
-
+	private String mapname;
 
 }
