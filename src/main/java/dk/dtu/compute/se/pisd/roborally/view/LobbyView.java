8b4f6d2d94d24f2111771ebca30b72e0a379cab7
Christian Livbjerg Worsøe <s235111@dtu.dk>
2024-06-25 12:55:31 +0200

    Improve LobbyView

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/LobbyView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/LobbyView.java
@@ -21,87 +21,76 @@
  */
 package dk.dtu.compute.se.pisd.roborally.view;
 
-import java.util.List;
-
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
 import dk.dtu.compute.se.pisd.roborally.controller.GameController;
 import dk.dtu.compute.se.pisd.roborally.controller.LobbyNetworkScheduler;
-import dk.dtu.compute.se.pisd.roborally.net.LobbyRest;
-import dk.dtu.compute.se.pisd.roborallyserver.model.Lobby;
 import dk.dtu.compute.se.pisd.roborallyserver.model.ServerPlayer;
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.util.Duration;
 
 /**
  * ...
  *
  * @author Ekkart Kindler, ekki@dtu.dk
- *
  */
 
 @Component
 public class LobbyView extends VBox implements ViewObserver {
 
+    private Label lobbyLabel;
+    private Button startButton;
+    private HBox top;
     private TableView<ServerPlayer> playerListView;
 
-    private Label lobbyLabel;
-    private Label statusLabel;
-
-    private Button startButton;
-
-
     public LobbyView(LobbyNetworkScheduler lns) {
+        lobbyLabel = new Label("Welcome to RoboRally lobby " + lns.getLobbyId());
+        lobbyLabel.setFont(new Font(16));
 
-
-        lobbyLabel = new Label("Welcome to the Roborally lobby");
-
-        playerListView = new TableView<ServerPlayer>();
+        playerListView = new TableView<>();
         playerListView.setItems(lns.playersFetched);
 
-        TableColumn<ServerPlayer,String> nameCol = new TableColumn<ServerPlayer,String>("Player name");
-        nameCol.setCellValueFactory(new PropertyValueFactory<ServerPlayer,String>("name"));
-        TableColumn<ServerPlayer,Integer> idCol = new TableColumn<ServerPlayer,Integer>("Player id");
-        idCol.setCellValueFactory(new PropertyValueFactory<ServerPlayer,Integer>("id"));
+        TableColumn<ServerPlayer, String> nameCol = new TableColumn<>("Player Name");
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn<ServerPlayer, Integer> idCol = new TableColumn<>("Player ID");
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
 
-        playerListView.getColumns().setAll(nameCol, idCol);
+        playerListView.getColumns().addAll(nameCol, idCol);
 
         lns.updatePlayersInLobby();
-
         lns.setPeriod(Duration.seconds(GameController.POLLING_RATE));
         lns.start();
 
-
-        startButton = new Button("Start game");
+        startButton = new Button("Start Game");
         startButton.setOnAction( e -> lns.requestStartGame() );
+        startButton.setFont(new Font(14));
+        startButton.setPadding(new Insets(4, 30, 4, 30));
 
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
 
+        top = new HBox(lobbyLabel, spacer, startButton);
+        top.setPadding(new Insets(6));
+        top.setAlignment(Pos.CENTER);
 
-        statusLabel = new Label("Current lobby: Lobby " + lns.getLobbyId().toString());
-
-        this.getChildren().add(lobbyLabel);
-        this.getChildren().add(playerListView);
-        this.getChildren().addAll(startButton);
-        this.getChildren().add(statusLabel);
-
+        this.getChildren().addAll(top, playerListView);
     }
-
 
     @Override
     public void updateView(Subject subject) {
         // TODO: Make this update view tick every once in a whilem and request new lobby info with REST
-
     }
-
 
 }
